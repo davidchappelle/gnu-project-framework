e2e5fef12887f73935af21e57f95aa430dcc2b62
David Chappelle <chappedm@gmail.com>
2014-06-05 13:39:28 -0400

    Updated library building to allow for appending object files and libraries
    This is useful for cases where you want to build a static library that is
    a superset of several smaller libraries. Or if you have a c++ library that
    wraps a c library this allows you to include the c library in the c++
    library so that you do not have to supply both to the linker.

--- mk/library.mk
+++ mk/library.mk
@@ -13,15 +13,16 @@ endif
 -include $(TOP_DIRECTORY)/mk/configuration.mk
 
 OBJECTS=$(addprefix $(BUILD_OUTPUT_DIRECTORY)/,$(addsuffix .o,$(basename $(SOURCES))))
+LIBRARIES=$(addprefix $(LIBRARY_DIRECTORY)/lib,$(addsuffix .a,$(LIBRARY_APPEND)))
 DEPENDENCIES=$(addprefix $(BUILD_OUTPUT_DIRECTORY)/,$(addsuffix .d,$(basename $(SOURCES))))
 LIBRARY_DIRECTORY=$(TOP_DIRECTORY)/lib
 LIBRARY_TARGET=$(LIBRARY_DIRECTORY)/lib$(LIBRARY).a
 BUILD_OUTPUT_FILES=$(OBJECTS) $(DEPENDENCIES) $(LIBRARY_TARGET)
 
-all: build-directories $(OBJECTS) $(LIBRARY_TARGET)
+all: build-directories $(OBJECTS) $(LIBRARIES) $(LIBRARY_TARGET)
 
-$(LIBRARY_TARGET): $(OBJECTS)
-	@$(AR) -r $(LIBRARY_TARGET) $(OBJECTS)
+$(LIBRARY_TARGET): $(ARCHIVE_OBJECTS)
+	@$(AR) -rcT $(LIBRARY_TARGET) $(OBJECTS) $(LIBRARIES)
 
 -include $(TOP_DIRECTORY)/mk/target.mk
 -include $(TOP_DIRECTORY)/mk/build.mk
